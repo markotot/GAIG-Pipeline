6341c9da56588ce2274d2a616c132269d46e55fc
markotot <m.tot@qmul.ac.uk>
2023-05-26 17:20:45 +0100

    feat: added hydra config

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import hydra
 import neptune
 from omegaconf import DictConfig
 
-NEPTUNE_PROJECT_NAME = "markotot/GAIG"
-
 
 @hydra.main(version_base=None, config_path="configs", config_name="config")
 def run_app(config: DictConfig) -> None:
-    run = neptune.init_run(project=NEPTUNE_PROJECT_NAME)
+
+    run = neptune.init_run(project=config.neptune.project_name)
 
     params = {"learning_rate": 0.002, "optimizer": "Adam"}
     run["parameters"] = params
