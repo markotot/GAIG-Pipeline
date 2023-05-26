0a921b6dd17e890611d44d1b6a6b66b4d07a0d05
markotot <m.tot@qmul.ac.uk>
2023-05-26 15:14:14 +0100

    feat: adding main.py

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import neptune
 
+NEPTUNE_PROJECT_NAME = "markotot/GAIG"
+NEPTUNE_API_TOKEN = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI4ODkxZmYyZC1lNzRlLTRlMTEtODJiNC1kYzNlNWQ5MWFlNDEifQ=="
+
+
 def run_app():
     run = neptune.init_run(
-        project="markotot/GAIG",
-        api_token="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI4ODkxZmYyZC1lNzRlLTRlMTEtODJiNC1kYzNlNWQ5MWFlNDEifQ==",
+        project=NEPTUNE_PROJECT_NAME, api_token=NEPTUNE_API_TOKEN
     )  # your credentials
 
     params = {"learning_rate": 0.002, "optimizer": "Adam"}
     run["parameters"] = params
 
     for epoch in range(10):
-        run["train/loss"].append(0.9 ** epoch)
+        run["train/loss"].append(0.9**epoch)
 
     run["eval/f1_score"] = 0.66
 
     run.stop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_app()
