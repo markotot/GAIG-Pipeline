b872e083403611233f13d916ded4c4319de3f30a
markotot <m.tot@qmul.ac.uk>
2023-05-27 15:35:54 +0100

    feat: modified the apptainer to run main.py

--- scripts/apocrita_run_app.sh
+++ scripts/apocrita_run_app.sh
@@ -6,7 +6,9 @@ set APOC_PASSWORD [lindex $argv 2];
 set APOC_PRIVATE_KEY [lindex $argv 3];
 
 spawn ssh -i $APOC_PRIVATE_KEY $APOC_USERNAME@login.hpc.qmul.ac.uk \
-"qsub example.sh"
+  "ls; \
+  whereis qsub; \
+  qsub example.sh"
 expect "Enter passphrase for key '$APOC_PRIVATE_KEY':"
 send "$APOC_PASSPHRASE\r"
 expect "$APOC_USERNAME@login.hpc.qmul.ac.uk's password"
