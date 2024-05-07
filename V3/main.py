7bb0fe5bccd69588dce66c8fc4c605d474302848
markotot <m.tot@qmul.ac.uk>
2024-05-07 14:53:13 +0100

    feat: changed the f1 eval

--- V3/main.py
+++ V3/main.py
@@ -22,7 +22,7 @@ def run_app(config: DictConfig) -> None:
     for step in range(config.training.training_steps):
         run["train/loss"].append(0.999**step)
 
-    run["eval/f1_score"] = 0.66
+    run["eval/f1_score"] = 0.80
     run["eval/total_time"] = "1h 23m 45s"
 
     run["parameters"] = params
