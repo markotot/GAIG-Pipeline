7075afe10aad03f00793e23821546f1f11d7b8d3
markotot <m.tot@qmul.ac.uk>
2024-05-07 10:45:11 +0100

    feat: added starting job on apocrita

--- V4.1/apocrita_run.sh
+++ V4.1/apocrita_run.sh
@@ -5,10 +5,18 @@ set APOC_PASSPHRASE [lindex $argv 1];
 set APOC_PASSWORD [lindex $argv 2];
 set APOC_PRIVATE_KEY [lindex $argv 3];
 
+set GIT_BRANCH [lindex $argv 4];
+set PROJECT_NAME [lindex $argv 5];
+set NEPTUNE_API_TOKEN [lindex $argv 6];
+
+
 spawn ssh -i $APOC_PRIVATE_KEY $APOC_USERNAME@login.hpc.qmul.ac.uk \
-  "
-    python3 test.py
-  "
+ "
+  source ../../../../../etc/bashrc; \
+  rm myenvs; \
+  echo NEPTUNE_API_TOKEN=$NEPTUNE_API_TOKEN > myenvs; \
+  qsub $PROJECT_NAME/V4.1/submit_run_job.sh ; \
+ "
 expect "Enter passphrase for key '$APOC_PRIVATE_KEY':"
 send "$APOC_PASSPHRASE\r"
 expect "$APOC_USERNAME@login.hpc.qmul.ac.uk's password"
