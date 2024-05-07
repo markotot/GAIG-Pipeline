972c1820b23a6ea827edace045795016d75f262d
markotot <m.tot@qmul.ac.uk>
2024-05-07 11:04:53 +0100

    fix added missing arguments to submit_job

--- V4.1/apocrita_run.sh
+++ V4.1/apocrita_run.sh
@@ -5,13 +5,13 @@ set APOC_PASSPHRASE [lindex $argv 1];
 set APOC_PASSWORD [lindex $argv 2];
 set APOC_PRIVATE_KEY [lindex $argv 3];
 
-set GIT_BRANCH [lindex $argv 4];
 set PROJECT_NAME [lindex $argv 5];
 set NEPTUNE_API_TOKEN [lindex $argv 6];
 
 
 spawn ssh -i $APOC_PRIVATE_KEY $APOC_USERNAME@login.hpc.qmul.ac.uk \
  "
+
   source ../../../../../etc/bashrc; \
   rm myenvs; \
   echo NEPTUNE_API_TOKEN=$NEPTUNE_API_TOKEN > myenvs; \
